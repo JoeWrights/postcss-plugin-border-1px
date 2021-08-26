7df5ae7961678daac797b319129c968bea78afa0
JoeWrights <1035208578@qq.com>
2021-08-26 20:31:16 +0800

    feat(*): complete plugin logic & add test case

--- index.js
+++ index.js
@@ -1,28 +1,92 @@
-module.exports = (opts = { }) => {
+module.exports = ({ ignoreCommentText = 'no' } = {}) => {
+  const afterPseudoExp = /(.*::?)(after)$/;
+  const beforePseudoExp = /(.*::?)(before)$/;
+  const BORDER_ATTRS = ["border", "border-left", "border-right", "border-top", "border-bottom", "border-width"];
+  let hasRelative = false;
 
-  // Work with options here
-
-  return {
-    postcssPlugin: 'PLUGIN_NAME',
-    /*
-    Root (root, postcss) {
-      // Transform CSS AST here
-    }
-    */
-
-    /*
-    Declaration (decl, postcss) {
-      // The faster way to find Declaration node
-    }
-    */
-
-    /*
-    Declaration: {
-      color: (decl, postcss) {
-        // The fastest way find Declaration node if you know property name
+  /**
+   * generate border style
+   * @param {*} selector selector
+   * @param {*} decl Declaration
+   * @returns {string} style block for different dpr
+   */
+  const geneBorderCommonStyle = (selector, decl = {}) => {
+    const genStyle = (radio = 1) => `
+    @media (-webkit-min-device-pixel-ratio: ${radio}), (min-device-pixel-ratio: ${radio}) {
+      ${selector} {
+        ${decl.prop}: ${decl.value};
+        position: absolute;
+        content: '';
+        top: 0;
+        left: 0;
+        -webkit-transform-origin: top left;
+        transform-origin: top left;
+        width: ${radio * 100}%;
+        height: ${radio * 100}%;
+        -webkit-transform: scale(${1 / radio});
+        transform: scale(${1 / radio});
+        pointer-events: none;
       }
     }
-    */
+  `;
+    const styles = [2, 3].reduce((acc, cur) => {
+      return `
+      ${acc}
+      ${genStyle(cur)}
+    `;
+    }, genStyle());
+    return styles;
+  };
+
+  return {
+    postcssPlugin: 'postcss-border1px',
+
+    Once(root) {
+      root.walkRules((rule) => {
+        hasRelative = false;
+        rule.walkDecls((decl) => {
+          const borderValues = decl.value.split(" ");
+
+          if (BORDER_ATTRS.indexOf(decl.prop) === -1) return;
+          if (borderValues[0] !== "1px") return;
+
+          // if find before pseudo
+          if (decl.parent && beforePseudoExp.test(decl.parent.selector)) return;
+
+          const curDeclIdx = rule.nodes.findIndex(({ prop }) => prop === decl.prop);
+          const nextNode = rule.nodes[curDeclIdx + 1];
+          const curDeclComment = (nextNode || {}).type === "comment" ? nextNode : null;
+
+          if (curDeclComment) {
+            // TODO: other comment
+            if (curDeclComment.text === ignoreCommentText) {
+              return;
+            }
+          } else {
+            rule.selectors = rule.selectors.reduce((acc, cur) => {
+              return !afterPseudoExp.test(cur) ? acc.concat(`${cur}:after`) : acc;
+            }, rule.selectors);
+
+            const afterPseudoSelector = rule.selectors.filter((selector) => afterPseudoExp.test(selector));
+
+            afterPseudoSelector.forEach((selector) => {
+              root.prepend(geneBorderCommonStyle(selector, decl));
+            });
+
+            rule.selectors = rule.selectors.slice(0, 1);
+            rule.removeChild(decl);
+
+            !hasRelative &&
+              rule.append({
+                prop: "position",
+                value: "relative"
+              });
+            hasRelative = true;
+          }
+        });
+      });
+    }
   }
 }
+
 module.exports.postcss = true
--- index.test.js
+++ index.test.js
@@ -2,16 +2,84 @@ const postcss = require('postcss')
 
 const plugin = require('./')
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
+async function run(input, output, opts = { ignoreCommentText: 'no' }) {
+  let result = await postcss([plugin(opts)]).process(input, {
+    from: undefined
+  })
+  expect(result.css.replace(/[ ]/g, '').replace(/[\r\n]/g, '')).toBe(output)
   expect(result.warnings()).toHaveLength(0)
 }
 
-/* Write tests here
-
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
+it(`dont't resolve`, async () => {
+  await run(
+    `.a { color: red; border: 2px solid red; }`,
+   `.a{color:red;border:2pxsolidred;}`)
 })
 
-*/
+it('resolve 1px', async () => {
+  await run(
+    `.a { color: red; border: 1px solid red; }`,
+   `@media(-webkit-min-device-pixel-ratio:1),(min-device-pixel-ratio:1){.a:after{border:1pxsolidred;position:absolute;content:'';top:0;left:0;-webkit-transform-origin:topleft;transform-origin:topleft;width:100%;height:100%;-webkit-transform:scale(1);transform:scale(1);pointer-events:none;}}@media(-webkit-min-device-pixel-ratio:2),(min-device-pixel-ratio:2){.a:after{border:1pxsolidred;position:absolute;content:'';top:0;left:0;-webkit-transform-origin:topleft;transform-origin:topleft;width:200%;height:200%;-webkit-transform:scale(0.5);transform:scale(0.5);pointer-events:none;}}@media(-webkit-min-device-pixel-ratio:3),(min-device-pixel-ratio:3){.a:after{border:1pxsolidred;position:absolute;content:'';top:0;left:0;-webkit-transform-origin:topleft;transform-origin:topleft;width:300%;height:300%;-webkit-transform:scale(0.3333333333333333);transform:scale(0.3333333333333333);pointer-events:none;}}.a{color:red;position:relative;}`)
+})
+
+it('resolve no comment', async () => {
+  await run(
+    `.a {
+      color: red;
+        border: 1px solid red; /*no*/
+    }`,
+    `.a{color:red;border:1pxsolidred;/*no*/}`
+  )
+})
+
+it('resolve other comment', async () => {
+  await run(
+    `.a {
+      color: red;
+        border: 1px solid red; /*ignore*/
+    }`,
+    `.a{color:red;border:1pxsolidred;/*ignore*/}`,
+    { ignoreCommentText: 'ignore' }
+  )
+})
+
+it('resolve ignore comment text', async () => {
+  await run(
+    `.a {
+      color: red;
+        border: 1px solid red; /*no*/
+    }`,
+    `.a{color:red;border:1pxsolidred;/*no*/}`
+  )
+})
+
+it('resolve other comment text with opts', async () => {
+  await run(
+    `.a {
+      color: red;
+        border: 1px solid red; /*other*/
+    }`,
+    `.a{color:red;border:1pxsolidred;/*other*/}`,
+    { ignoreCommentText: 'ignore' }
+  )
+})
+
+it('resolve after pseudo with no comment', async () => {
+  await run(
+    `.a:after {
+      color: red;
+        border: 1px solid red; /*no*/
+    }`,
+    `.a:after{color:red;border:1pxsolidred;/*no*/}`
+  )
+})
+
+it('resolve before pseudo', async () => {
+  await run(
+    `.a:before {
+      color: red;
+        border: 1px solid red;
+    }`,
+    `.a:before{color:red;border:1pxsolidred;}`
+  )
+})
